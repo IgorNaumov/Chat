ef1bdf73974cd23221f597ea250a71725b732bdc
Igor <il.naumov@bk.ru>
2021-05-19 20:57:05 +0300

    update People

---
+++ Chat/Extantion/UIKit/UIViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UIVieController.swift
+//  Chat
+//
+//  Created by nil on 19.05.2021.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+     func configure<T: SelfConfiguringCell, U: Hashable>(collectionView: UICollectionView, cellType: T.Type, with value: U, for indexPath: IndexPath) -> T {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellType.reuseId, for: indexPath) as? T else {fatalError("Unable to dequeue \(cellType)") }
+        cell.configure(with: value)
+        return cell
+    }
+}
